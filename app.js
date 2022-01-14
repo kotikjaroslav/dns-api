a22468753d4f99c097dda3fd8d3c9b4ff9aae6c5
bc29ea3c101054baa1429ffc2edba4ae <kotik.jaroslav@gmail.com>
2022-01-14 19:06:20 +0100

    make it better ;)

---
+++ app.js
@@ -0,0 +1,28 @@
+const CONFIG = {
+    port: 3000
+}
+
+const dns       = require('dns')
+const express   = require('express')
+const app       = express()
+
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    console.log('req >> / ')
+    res.status(200).json(`Welcome to DNS API`)
+});
+
+app.post('/resolve', (req, res) => {
+    let domain = req.body.domain
+    let type = req.body.type
+
+    dns.resolve(domain,type.toUpperCase(),(err,records)=>{
+        res.status(200).json(records)
+    });
+});
+
+
+app.listen(CONFIG.port, () => {
+    console.log(`the server is listening on port ${CONFIG.port}`)
+})
---
+++ test.sh
@@ -0,0 +1,6 @@
+
+DATA='{"domain":"mall.cz","type":"ANY"}'
+
+
+curl -H 'Content-Type: application/json' -X POST http://127.0.0.1:3000 -d $DATA
+
